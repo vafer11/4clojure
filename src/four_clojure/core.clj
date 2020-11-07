eeab7a6c9f1e291fe8d328d1bc37f3a7cf3e8470
vafer11 <vfernandez11@outlook.com>
2020-11-07 18:34:16 -0300

    Adding test

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -1,32 +1,32 @@
 (ns four-clojure.core)
 
 ; A nil Key: Write a function which, given a key and map, returns true iff the map contains an entry with that key and its value is nil.
-(fn [k m]
+(defn a-nil-key [k m]
   (if (contains? m k)
   (nil? (k m))
   false))
 
-;Default Map: Write a function which takes a default value and a sequence of keys and constructs a map.
-(fn [v s]
+; Default Map: Write a function which takes a default value and a sequence of keys and constructs a map.
+(defn default-map [v s]
   (reduce
    #(conj %1 {%2 v})
    {}
    s))
 
-; Write a function which returns the last element in a sequence.
-(fn [s]
+; Get-last: Write a function which returns the last element in a sequence.
+(defn get-last [s]
   (first (reverse s)))
 
 ; Penultimate Element: Write a function which returns the second to last element from a sequence.
-(fn [s]
+(defn get-penultimate [s]
   (second (reverse s)))
 
 ; Nth Element: Write a function which returns the Nth element from a sequence.
-(fn [s n]
+(defn get-nth [s n]
   (first (drop n s)))
 
 ; Count a Sequence: Write a function which returns the total number of elements in a sequence.
-(fn [s]
+(defn count-seq [s]
   (reduce
    (fn [c v]
      (inc c))
@@ -34,7 +34,7 @@
    s))
 
 ; Sum It All Up: Write a function which returns the sum of a secuence number
-(fn [s]
+(defn sum-it-all [s]
   (reduce + s))
 
 ; Find the odd numbers: Write a function which returns only the odd number of a secuence 
@@ -42,11 +42,8 @@
   (filter odd? s))
 
 ; Reverse a Sequence: Write a function which reverses a secuence
-(fn [s]
-  (let [r (into '() s)]
-    (if (vector? s)
-      (vec r)
-      r)))
+(defn reverse-seq [s]
+  (into '() s))
 
 ; Polindrome Detector: Write a function which returns true if the given sequence is a polindrome 
 (fn [s]
@@ -123,11 +120,19 @@
 
 ; Drop Every Nth Item: Write a function which drops every Nth item from a sequence
 (fn [s v]
-  (filter 
-   #(not= 0 (mod (inc (.indexOf s %)) v)) 
-   s))
+  (->> (map list (range 1 (inc (count s))) s)
+       (filter #(not= 0 (mod (first %1) v)))
+       (map last)))
 
 ; Spit a Sequence: Write a function which slips a sequence into two parts. (split-at restricted)
 (fn [v s]
   (let [seq-size (count s) n (- seq-size v)]
     (list (take v s) (take-last n s))))
+
+; Pack a Sequence: Write a fuction which packs consecutive duplicates into sub-list
+(fn [s]
+  (partition-by identity s))
+
+; Map Construction: Write a function which takes a vector of keys and a vector values and constracts a map from them.
+(fn [k v]
+  (apply merge (map hash-map k v)))
--- test/four_clojure/core_test.clj
+++ test/four_clojure/core_test.clj
@@ -2,6 +2,42 @@
   (:require [clojure.test :refer :all]
             [four-clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest a-nil-key-test
+  (is (= true (a-nil-key :a {:a nil :b 2})))
+  (is (= false (a-nil-key :c {:a 1 :b 2})))
+  (is (= false (a-nil-key :c {:a 1 :b 2 :c 3}))))
+
+(deftest default-map-test
+  (is (= {1 "a" 2 "a" 3 "a"} (default-map "a" [1 2 3])))
+  (is (= {:a 0 :b 0 :c 0} (default-map 0 [:a :b :c])))
+  (is (= {:a [1 2] :b [1 2]} (default-map [1 2] [:a :b]))))
+
+(deftest get-last-test
+  (is (= 2 (get-last '(1 3 2))))
+  (is (= 5 (get-last [1 2 3 4 5])))
+  (is (= [1 2] (get-last [1 2 [1 2] 3 [1 2]]))))
+
+(deftest get-penultimate-test
+  (is (= 4 (get-penultimate '(1 2 3 4 5))))
+  (is (= 2 (get-penultimate [1 2 3])))
+  (is (= [1 2] (get-penultimate [[1 2] 2]))))
+
+(deftest get-nth-test
+  (is (= 1 (get-nth '(1 2 3 4 5) 0)))
+  (is (= 5 (get-nth '(1 2 3 4 5) 4)))
+  (is (= [1 2] (get-nth [1 [1 2] [3 4]] 1))))
+
+(deftest count-seq-test
+  (is (= 5 (count-seq [1 2 5 6 7])))
+  (is (= 3 (count-seq (list 1 2 3))))
+  (is (= 0 (count-seq []))))
+
+(deftest sum-it-all-test
+  (is (= 7 (sum-it-all [0 5 2 0 0])))
+  (is (= 0 (sum-it-all [])))
+  (is (= 21 (sum-it-all [1 2 3 4 5 6]))))
+
+(deftest reverse-seq-test
+  (is (= [5 4 3 2 1] (reverse-seq [1 2 3 4 5])))
+  (is (= '(7 5 2) (reverse-seq (sorted-set 7 5 7 2))))
+  (is (= [[1 2] [3 4] [5 6]] (reverse-seq [[5 6] [3 4] [1 2]]))))
