8a26068a02c6a33e2775316f7d221f57b97bf6b4
vafer11 <vfernandez11@outlook.com>
2020-12-16 23:23:41 -0300

    Adding new solutions and test implementation.

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -1,103 +1,131 @@
 (ns four-clojure.core)
 
-; A nil Key: Write a function which, given a key and map, returns true iff the map contains an entry with that key and its value is nil.
-(defn a-nil-key [k m]
+(defn a-nil-key
+  "A nil Key: Given a key and map, returns true iff the map contains an entry with that key and its value is nil."
+  [k m]
   (if (contains? m k)
   (nil? (k m))
   false))
 
-; Default Map: Write a function which takes a default value and a sequence of keys and constructs a map.
-(defn default-map [v s]
+(defn default-map
+  "Default Map: Takes a default value and a sequence of keys and constructs a map."
+  [v s]
   (reduce
    #(conj %1 {%2 v})
    {}
    s))
 
-; Get-last: Write a function which returns the last element in a sequence.
-(defn get-last [s]
+(defn get-last
+  "Get-last: Returns the last element in a sequence."
+  [s]
   (first (reverse s)))
 
-; Penultimate Element: Write a function which returns the second to last element from a sequence.
-(defn get-penultimate [s]
+(defn get-penultimate
+  "Penultimate Element: Returns the second to last element from a sequence."
+  [s]
   (second (reverse s)))
 
-; Nth Element: Write a function which returns the Nth element from a sequence.
-(defn get-nth [s n]
+(defn get-nth
+  "Nth Element: Returns the Nth element from a sequence."
+  [s n]
   (first (drop n s)))
 
-; Count a Sequence: Write a function which returns the total number of elements in a sequence.
-(defn count-seq [s]
+(defn count-seq
+  "Count a Sequence: Returns the total number of elements in a sequence."
+  [s]
   (reduce
    (fn [c v]
      (inc c))
    0
    s))
 
-; Sum It All Up: Write a function which returns the sum of a secuence number
-(defn sum-it-all [s]
+(defn sum-it-all
+  "Sum It All Up: Returns the sum of a sequence number."
+  [s]
   (reduce + s))
 
-; Find the odd numbers: Write a function which returns only the odd number of a secuence 
-(fn [s]
+(defn odd-numbers
+  "Find the odd numbers: Returns only the odd number of a sequence."
+  [s]
   (filter odd? s))
 
-; Reverse a Sequence: Write a function which reverses a secuence
-(defn reverse-seq [s]
+(defn reverse-seq
+  "Reverse a Sequence: Reverses a sequence."
+  [s]
   (into '() s))
 
-; Polindrome Detector: Write a function which returns true if the given sequence is a polindrome 
-(fn [s]
+(defn palindrome-detector
+  "Palindrome Detector: Returns true if the given sequence is a palindrome."
+  [s]
   (if (contains? [0 1] (count s))
     true
     (and (= (first s) (last s)) (recur (drop-last (rest s))))))
 
-; Fibonacci Sequence: Write a function which returns the first X fibonacci numbers
-(fn [n]
+(defn fibonacci-seq
+  "Fibonacci Sequence: Returns the first X fibonacci numbers."
+  [n]
   (take n
         (reduce (fn [l _]
                   (conj l (apply + (take-last 2 l)))) 
                 [1 1] 
                 (range n))))
 
-; Maximum Number: Write a function which takes a variable number of parameters and returns the maximun number
-(fn [& n] 
+(defn maximum-number
+  "Maximum Number: Takes a variable number of parameters and returns the maximum number."
+  [& n]
   (last (sort n)))
 
-; Get the Caps: Write a function which takes a string and returns a new string containing only the capital letters
-(fn [s]
+(defn get-the-caps
+  "Get the Caps: Takes a string and returns a new string containing only the capital letters."
+  [s]
   (->> s
        (filter #(Character/isUpperCase %))
        (apply str)))
 
-; Duplicate a Sequence: Write a function which duplicates each element of a sequence
-(fn [s]
+(defn duplicate-seq
+  "Duplicate a Sequence: Duplicates each element of a sequence."
+  [s]
   (->> s
        (map #(repeat 2 %))
        (apply concat)))
 
-;Compress a Sequence: Write a function which removes consecutive duplicates from a sequence
-(fn [s]
-  (->> s
-       (dedupe)
-       (apply str)))
+(defn set-intersection
+  "Set Intersection: Returns the intersection of two sets."
+  [set1 set2]
+  (set (filter #(contains? set1 %) set2)))
 
-; Implement range: Write a function which creates a list of all integers in a given range.
-(fn [initial stop]
+(defn compress-seq
+  "Compress a Sequence: Removes consecutive duplicates from a sequence."
+  [s]
+  (reduce
+    #(if (= (last %1) %2)
+       %1
+       (conj %1 %2))
+    []
+    s))
+
+
+(defn rrange
+  "Implement range: Creates a list of all integers in a given range."
+  [initial stop]
   (loop [result [] e initial]
     (if (= e stop)
       result
       (recur (conj result e) (inc e)))))
 
-; Factorial Fun: Write a function which calculates factorials.
-(fn [n]
-  (->> n (+ 1) (range 1) (apply *)))
+(defn get-factorial
+  "Factorial Fun: Calculates factorials."
+  [n]
+  (->> (+ n 1) (range 1) (apply *)))
 
-; Interleave Two Seqs: Write a function which takes two sequences and returns the first item from each, then the second item from each, then the third, etc.
-(fn [s1 s2]
+(defn interleave-two-seqs
+  "Interleave Two Seqs: Takes two sequences and returns the first item from each, then the second item from each, then the third, etc."
+  [s1 s2]
   (mapcat list s1 s2))
 
-; Flatten a Sequence: Write a functon which flatten a sequence
-(fn [s]
+(defn flatten-seq
+  "Flatten a Sequence: Flatten a sequence."
+  [s]
   (loop [acc [] lis s]
     (if (empty? lis)
       acc
@@ -106,33 +134,57 @@
           (recur acc (concat f r))
           (recur (conj acc f) r))))))
 
-; Replicate a Sequence: Write a function which each element of a sequance variable of a number time
-(fn [s n]
+(defn replicate-seq
+  "Replicate a Sequence: Replicates each element of a sequence variable of a number time."
+  [s n]
   (->> s
        (map #(repeat n %))
        (apply concat)))
 
-; Interpose a Seq: Write a function which separate the items by an arbitrary value
-(fn [v s]
+(defn interpose-seq
+  "Interpose a Seq: Separate the items by an arbitrary value."
+  [v s]
   (->> s
        (mapcat #(list % v))
        (drop-last)))
 
-; Drop Every Nth Item: Write a function which drops every Nth item from a sequence
-(fn [s v]
+(defn drop-nth-item
+  "Drop Every Nth Item: Drops every Nth item from a sequence."
+  [s v]
   (->> (map list (range 1 (inc (count s))) s)
        (filter #(not= 0 (mod (first %1) v)))
        (map last)))
 
-; Spit a Sequence: Write a function which slips a sequence into two parts. (split-at restricted)
-(fn [v s]
+(defn split-seq
+  "Spit a Sequence: Slips a sequence into two parts. (split-at restricted)."
+  [v s]
   (let [seq-size (count s) n (- seq-size v)]
     (list (take v s) (take-last n s))))
 
-; Pack a Sequence: Write a fuction which packs consecutive duplicates into sub-list
-(fn [s]
+(defn pack-seq
+  "Pack a Sequence: Packs consecutive duplicates into sub-list."
+  [s]
   (partition-by identity s))
 
-; Map Construction: Write a function which takes a vector of keys and a vector values and constracts a map from them.
-(fn [k v]
+(defn map-construction
+  "Map Construction: Takes a vector of keys and a vector values and constructs a map from them."
+  [k v]
   (apply merge (map hash-map k v)))
+
+(defn greatest-common-divisor
+  "Greatest Common Divisor: Given two integers, returns the greatest common divisor."
+  [a b]
+  (->> (max a b)
+       (range 1)
+       (filter #(and (= 0 (mod a %)) (= 0 (mod b %))))
+       (apply max)))
+
+(defn function-composition
+  "Function Composition: Allows you to create function compositions. The parameter list should take a variable number of functions, applies them from right-to-left."
+  [& functions]
+  (fn [& params]
+    (let [rev (reverse functions)]
+      (reduce
+        #(%2 %1)
+        (apply (first rev) params)
+        (rest rev)))))
--- test/four_clojure/core_test.clj
+++ test/four_clojure/core_test.clj
@@ -41,3 +41,98 @@
   (is (= [5 4 3 2 1] (reverse-seq [1 2 3 4 5])))
   (is (= '(7 5 2) (reverse-seq (sorted-set 7 5 7 2))))
   (is (= [[1 2] [3 4] [5 6]] (reverse-seq [[5 6] [3 4] [1 2]]))))
+
+(deftest palindrome-detector-test
+  (is (= true (palindrome-detector [1 2 2 1])))
+  (is (= false (palindrome-detector "abab")))
+  (is (= true (palindrome-detector ""))))
+
+(deftest fibonacci-seq-test
+  (is (= [1 1 2 3 5 8] (fibonacci-seq 6)))
+  (is (= [1 1 2 3 5 8 13 21] (fibonacci-seq 8)))
+  (is (= [] (fibonacci-seq 0))))
+
+(deftest maximum-number-test
+  (is (= 80 (maximum-number 6 6 7 80)))
+  (is (= 20 (maximum-number 8 5 7 0 20)))
+  (is (= 0 (maximum-number 0 0 0))))
+
+(deftest get-the-caps-test
+  (is (= "ABT" (get-the-caps "ABrT1")))
+  (is (= "HW" (get-the-caps "Hello Word")))
+  (is (= "" (get-the-caps ""))))
+
+(deftest duplicate-seq-test
+  (is (= [1 1 2 2 5 5] (duplicate-seq [1 2 5])))
+  (is (= '("a" "a" "c" "c" "c" "c") (duplicate-seq (list "a" "c" "c"))))
+  (is (= [[1] [1] #{1} #{1}] (duplicate-seq [[1] #{1}]))))
+
+(deftest set-intersection-test
+  (is (= #{2 3} (set-intersection #{0 1 2 3} #{2 3 4 5})))
+  (is (= #{ } (set-intersection #{0 1 2} #{3 4 5})))
+  (is (= #{:a :c :d} (set-intersection #{:a :b :c :d} #{:c :e :a :f :d}))))
+
+(deftest compress-seq-test
+  (is (= [1 2 3] (compress-seq [1 1 1 2 3 3])))
+  (is (= "abc" (apply str (compress-seq "aabbcc"))))
+  (is (= [[1 2] [1 2 3] [4 5]]) (compress-seq [[1 2] [1 2] [1 2 3] [4 5] [4 5]])))
+
+(deftest rrange-test
+  (is (= [0 1 2 3 4 5 6 7 8 9] (rrange 0 10)))
+  (is (= '[] (rrange 0 0)))
+  (is (= [-1 0 1 2 3] (rrange -1 4))))
+
+(deftest get-factorial-test
+  (is (= 120 (get-factorial 5)))
+  (is (= 5040 (get-factorial 7)))
+  (is (= 6 (get-factorial 3))))
+
+(deftest interleave-two-seqs-test
+  (is (= '(1 :a 2 :b 3 :c) (interleave-two-seqs [1 2 3] [:a :b :c])))
+  (is (= '([1] 1 [2] 2 [3] 3) (interleave-two-seqs [[1] [2] [3]] (list 1 2 3))))
+  (is (= '(1 "A" 2 "B" 3 "C") (interleave-two-seqs [1 2 3] (list "A" "B" "C")))))
+
+(deftest flatten-seq-test
+  (is (= [1 2 4 5 6 7 8] (flatten-seq [1 [2] [4 5 6] [[[7]]8]])))
+  (is (= '(4 :a 5 :v 7 8) (flatten-seq [4 [:a 5 [:v 7]] 8])))
+  (is (= [] (flatten-seq [[[[[]] [[[[]]]]]]]))))
+
+(deftest replicate-seq-test
+  (is (= [] (replicate-seq [1 2 3] 0)))
+  (is (= [[] [] [] "A" "A" "A" "B" "B" "B"] (replicate-seq [[] "A" "B"] 3)))
+  (is (= [] (replicate-seq [] 5))))
+
+(deftest interpose-seq-test
+  (is (= [] (interpose-seq "," [])))
+  (is (= ["a" ";" "b" ";" "c"] (interpose-seq ";" ["a" "b" "c"])))
+  (is (= [:a :Z :b :Z :c] (interpose-seq :Z [:a :b :c]))))
+
+(deftest drop-nth-item-test
+  (is (= [1 3 5 7 9] (drop-nth-item [1 2 3 4 5 6 7 8 9] 2)))
+  (is (= [[1 2] [3 4 5] [4 5]] (drop-nth-item [[1 2] [3 4 5] [5 6] [4 5]] 3)))
+  (is (= [] (drop-nth-item [1 2 3 4 5 6 7 8 9 10] 1))))
+
+(deftest split-seq-test
+  (is (= '((1 2 3 4 5) (6 7 8 9 0)) (split-seq 5 [1 2 3 4 5 6 7 8 9 0])))
+  (is (= '(() (1 2 3 4 5)) (split-seq 0 [1 2 3 4 5])))
+  (is (= '((6 7) (8 9 0)) (split-seq 2 [6 7 8 9 0]))))
+
+(deftest pack-seq-test
+  (is (= '((1 1) (2 2) (4) (5 5)) (pack-seq [1 1 2 2 4 5 5])))
+  (is (=  '((:A :A) (:B) (:c :c) (:D)) (pack-seq [:A :A :B :c :c :D])))
+  (is (= ['(1)] (pack-seq [1]))))
+
+(deftest map-construction-test
+  (is (= {:a 1 :b 2 :c 3} (map-construction [:a :b :c] [1 2 3])))
+  (is (=  {1 "one" 2 "two" 3 "three"} (map-construction [1 2 3 4] ["one" "two" "three"])))
+  (is (=  {:foo "foo" :bar "bar"} (map-construction [:foo :bar] ["foo" "bar" "baz"]))))
+
+(deftest greatest-common-divisor-test
+  (is (=  3 (greatest-common-divisor 3 6)))
+  (is (= 40 (greatest-common-divisor 120 80)))
+  (is (= 33 (greatest-common-divisor 1023 858))))
+
+(deftest function-composition-test
+  (is (= [3 2 1] ((function-composition rest reverse) [1 2 3 4])))
+  (is (= 5 ((function-composition (partial + 3) second) [1 2 3 4])))
+  (is (= true ((function-composition zero? #(mod % 8) +) 3 5 7 9))))
